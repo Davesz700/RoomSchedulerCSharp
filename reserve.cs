c0fd48e3f5a5c2364373cfdc3d9a62f0f303353b
Nicolas <nicolas.david@unifesp.br>
2026-05-11 17:34:27 -0300

    criando métodos para reserva

--- reserve.cs
+++ reserve.cs
@@ -1,23 +1,51 @@
-
-
-public class reserve
+namespace ReservaSalas.core
 {
-    private int Id { get; set; }
-    private int ClassroomId { get; set; }
-    private int UserId { get; set; }
-    public DateTime Date { get; set; }
-    private TimeSpan StartTime { get; set; }
-    private TimeSpan EndTime { get; set; }
+    
 
-        public reserve(int id, int classroomId, int userId, DateTime date, TimeSpan startTime, TimeSpan endTime)
+    public class reserve
+    {
+        public int Id { get; private set; }
+        public int SalaId { get; private set; }
+        public int UsuarioId { get; private set; }
+        public DateTime Data { get; private set; }
+        public TimeSpan Inicio { get; private set; }
+        public TimeSpan Fim { get; private set; }
+        public bool Ativa { get; private set; } = true;
+
+        // Serviços adicionais opcionais Decorator
+        private readonly List<string> _servicosExtras = new();
+        public IReadOnlyList<string> ServicosExtras => _servicosExtras;
+
+        public Reserva(int id, int salaId, int usuarioId, DateTime data, TimeSpan inicio, TimeSpan fim)
         {
+            if (fim <= inicio)
+                throw new ArgumentException("Horário de fim deve ser posterior ao início.");
+
             Id = id;
-            ClassroomId = classroomId;
-            UserId = userId;
-            Date = date;
-            StartTime = startTime;
-            EndTime = endTime;
+            SalaId = salaId;
+            UsuarioId = usuarioId;
+            Data = data;
+            Inicio = inicio;
+            Fim = fim;
         }
 
+        public void Cancelar() => Ativa = false;
 
+        public void Modificar(DateTime novaData, TimeSpan novoInicio, TimeSpan novoFim)
+        {
+            if (novoFim <= novoInicio)
+                throw new ArgumentException("Horário de fim deve ser posterior ao início.");
+            Data = novaData;
+            Inicio = novoInicio;
+            Fim = novoFim;
+        }
+
+        public bool ColideComOutra(Reserva outra)
+        {
+            if (outra.SalaId != SalaId || outra.Data.Date != Data.Date)
+                return false;
+            return Inicio < outra.Fim && Fim > outra.Inicio;
+        }
+
+    }
 }
